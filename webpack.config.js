96f3e83ddda8288f6ffb9ced06dc07a1a4a7df8b
Michael K <keidarster@gmail.com>
2018-03-06 15:47:39 +0100

    added some pug variables

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,14 @@ const path = require("path");
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: './dist/bundle.js'
+	filename: './dist/bundle.js'
 	},
 		plugins: [
  			new HtmlWebpackPlugin({
    			template: '!!pug-loader!src/index.pug',
+   			xesc: 'A very long descriptions with test and bla',
+   			billing: 'Billing address und blablabla',
+   			posts: ["post1", "post2", "post3", "post4"]
  		}),
 	]
 };
