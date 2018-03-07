818ea6ec4da18540c89a43e7ffadc75377e10f7a
Michael K <keidarster@gmail.com>
2018-03-07 13:35:37 +0100

    added react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import $ from 'jquery';
 import 'bootstrap';
-console.log("hey");
+console.log("hey"); 
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1 className="hello">Hello World</h1>
+				<h3>Something blabla</h3>
+			</div>
+		);
+	}
+}
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-	filename: './dist/bundle.js'
-	},
-		plugins: [
- 			new HtmlWebpackPlugin({
-   			template: '!!pug-loader!src/index.pug',
-   			xesc: 'A very long descriptions with test and bla',
-   			billing: 'Billing address und blablabla',
-   			posts: ["post1", "post2", "post3", "post4"]
- 		}),
-	]
+    entry: './src/index.js',
+    output: {
+        filename: './dist/bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }, ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: '!!pug-loader!src/index.pug',
+            xesc: 'A very long descriptions with test and bla',
+            billing: 'Billing Address',
+            posts: ["post1", "post2", "post3", "post4"]
+        }),
+    ]
 };
-
