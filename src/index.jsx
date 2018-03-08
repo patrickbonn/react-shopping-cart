dcc4dc1f1e9cd18cda6dd12adbfc3ee7f482e038
Michael K <keidarster@gmail.com>
2018-03-08 13:40:31 +0100

    extracted Product to a single component

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import $ from 'jquery';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Product from './Product.jsx';
 
 class App extends React.Component {
 	constructor(props) {
@@ -41,23 +42,5 @@ class App extends React.Component {
 	}
 }
 
-class Product extends React.Component {
-	render() {
-	  return (
-	        <li className="list-group-item d-flex justify-content-between lh-condensed">
-	          <div>
-	            <h6 className="my-0">{ this.props.data.name }</h6>
-	            <small className="text-muted">Release: { this.props.data.release }</small>
-	          </div>
-	          <div className="d-flex flex-column">
-	            <span className="text-muted d-flex justify-content-center align-items-baseline">
-	              <button className="btn btn-sm btn-light" >-</button>
-	              <button className="btn btn-sm btn-light">+</button>
-	            </span>
-	            <span className= "badge" >{ this.props.data.amount }</span>
-	          </div>
-	        </li>
-	  );
-	}
-}
+
 ReactDOM.render(<App/>, document.getElementById('root'));
