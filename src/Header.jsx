dd74f78c4c10011149e9f50365870ff4ccacfd4f
unknown <patrick.bonn@yahoo.de>
2018-03-13 10:49:23 +0100

    shopping cart start with react

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
-class Header extends React.Component {
-	render() {
-		return (
-			<div className="py-5 text-center">
-				<img className="d-block mx-auto mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
-					<h2>Checkout form yo</h2>
-					<h1>The problem of the future - today!</h1>
-					<ul className="list-unstyled">
-						<li>post1</li>
-						<li>post2</li>
-						<li>post3</li>
-						<li>post4</li>
-					</ul>
-					<p className="lead">Below is an example form built entirely with Bootstrap's form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.
-					</p>
-			</div>	
-		);
-	}
+function Header (props) {
+	return (
+		<div className="py-5 text-center">
+			<img className="d-block mx-auto mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
+				<h2>Checkout form yo</h2>
+				<h1>The problem of the future - today!</h1>
+				<ul className="list-unstyled">
+					<li>post1</li>
+					<li>post2</li>
+					<li>post3</li>
+					<li>post4</li>
+				</ul>
+				<p className="lead">Below is an example form built entirely with Bootstrap's form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.
+				</p>
+		</div>	
+	);
 }
+Header(props);
+
 
 export default Header;
 
